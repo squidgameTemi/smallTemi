bf26b17aecfd9dae0c3133fe9f365058eb9d1fce
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2023-05-24 19:02:10 +0900

    Create layout & Add skidjoy feature
    Create layout and Add skidjoy

--- app/src/main/java/com/example/smalltemi/MainActivity.java
+++ app/src/main/java/com/example/smalltemi/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.smalltemi;
 
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.robotemi.sdk.Robot;
+import com.robotemi.sdk.listeners.OnRobotReadyListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        OnRobotReadyListener {
 
 
 
     FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+    Robot robot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void skidJoy(View view) {
+        long t = System.currentTimeMillis();
+        long end = t + 4000;
+        while (System.currentTimeMillis() < end) {
+            robot.skidJoy(1F, 0F);
+            if(System.currentTimeMillis() == end - 2000){
+                break;
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        robot.addOnRobotReadyListener(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        robot.removeOnRobotReadyListener(this);
+    }
+
+    public void onRobotReady(boolean isReady) {
+        if (isReady) {
+            try {
+                final ActivityInfo activityInfo = getPackageManager().getActivityInfo(getComponentName(), PackageManager.GET_META_DATA);
+                // Robot.getInstance().onStart() method may change the visibility of top bar.
+                robot.onStart(activityInfo);
+            } catch (PackageManager.NameNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
 }
